aecc3635820cbd5b3b4243487df4eeb405ce89a0
Long vv <longvv@bap.jp>
2024-06-24 11:27:06 +0700

    "allure extend results"

--- src/main/java/com/reports/ExtentReportManager.java
+++ src/main/java/com/reports/ExtentReportManager.java
@@ -1,5 +1,6 @@
 package com.reports;
 
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.constants.ConstantGlobal;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
@@ -10,6 +11,7 @@ public class ExtentReportManager {
 
     public synchronized static ExtentReports getExtentReports() {
         ExtentSparkReporter reporter = new ExtentSparkReporter(ConstantGlobal.EXTENT_REPORT_PATH);
+        reporter.config().setTheme(Theme.STANDARD);
         reporter.config().setReportName("Extent Report | Hoan Tester");
         extentReports.attachReporter(reporter);
         extentReports.setSystemInfo("Framework Name", "Selenium Java Course 04/2022 | Hoan Tester");
